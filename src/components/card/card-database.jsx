363a844233a86ee0618ba01254daee4d67799448
Vict Mart <codigovictor01@gmail.com>
2022-09-07 18:25:05 +0200

    edit

--- src/components/card/card-database.jsx
+++ src/components/card/card-database.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { AiFillEdit } from "react-icons/ai";
 import { AiFillDelete } from "react-icons/ai";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function CardDataBase() {
   const [emociones, setEmociones] = useState([]);
@@ -40,23 +42,31 @@ function CardDataBase() {
         title: e.nuevoTitulo,
         emotionsManage: e.nuevaDescripcion.split("\n").filter(Boolean),
       }),
-    }).then(() =>
-      // window.location.reload(false)
-      {}
-    );
+    }).then(() => {
+      window.location.reload(false);
+      // e.title = e.nuevoTitulo;
+      // e.emotionsManage = e.nuevaDescripcion.split("\n").filter(Boolean);
+      // console.log(emociones);
+      // desactivarEdicion(e);
+      // setEmociones(emociones);
+    });
   }
 
-  async function activarEdicion(e) {
+  function activarEdicion(e) {
     e.edicionActivada = true;
     e.nuevoTitulo = "";
     e.nuevaDescripcion = "";
     console.log(e);
+    document.getElementById(e._id).classList.add("enabled");
+    document.getElementById(e._id).classList.remove("disabled");
   }
 
-  async function desactivarEdicion(e) {
+  function desactivarEdicion(e) {
     e.edicionActivada = false;
     e.nuevoTitulo = null;
     e.nuevaDescripcion = null;
+    document.getElementById(e._id).classList.add("disabled");
+    document.getElementById(e._id).classList.remove("enabled");
   }
 
   return (
@@ -74,48 +84,56 @@ function CardDataBase() {
 
           <Card.Body>
             <Card.Title>{emocion.title}</Card.Title>
-            {emocion.emotionsManage?.map(function (parrafo) {
-              return <Card.Text className="card_text">{parrafo}</Card.Text>;
+            {emocion.emotionsManage.map(function (parrafo) {
+              return (
+                <Card.Text key={parrafo} className="card_text">
+                  {parrafo}
+                </Card.Text>
+              );
             })}
             <AiFillEdit onClick={() => activarEdicion(emocion)} /> &nbsp; &nbsp;
-            <AiFillDelete onClick={() => eliminarEmocion(emocion)} />{" "}
-            {emocion.edicionActivada}
-            {emocion.edicionActivada == true ? (
-              <div>
-                <form className="formi">
-                  <h1>Your Emotion card</h1>
-                  <label>
-                    Title:
-                    <br />
-                    <input
-                      onChange={(e) => {
-                        emocion.nuevoTitulo = e.target.value;
-                      }}
-                    />
-                  </label>
-                  <label>
-                    Description:
-                    <br />
-                    <textarea
-                      onChange={(e) => {
-                        e.nuevaDescripcion = e.target.value;
-                      }}
-                    />
-                  </label>
-                  <button onClick={() => editarEmocion(emocion)} type="button">
-                    Submit
-                  </button>
-                  <button
-                    onClick={() => desactivarEdicion(emocion)}
-                    type="button"
-                  >
-                    Cancel
-                  </button>
-                </form>
-              </div>
-            ) : (
-              <div>Hacer prueba</div>
-            )}
+            <AiFillDelete onClick={() => eliminarEmocion(emocion)} />
+            <div className="disabled" id={emocion._id}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  onChange={(e) => {
+                    emocion.nuevoTitulo = e.target.value;
+                  }}
+                  type="text"
+                  placeholder="Enter text"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={7}
+                  onChange={(e) => {
+                    emocion.nuevaDescripcion = e.target.value;
+                  }}
+                />
+              </Form.Group>
+              <Form>
+                <Button
+                  onClick={() => editarEmocion(emocion)}
+                  variant="primary"
+                  type="button"
+                >
+                  Update
+                </Button>
+                <Button
+                  onClick={() => desactivarEdicion(emocion)}
+                  variant="secondary"
+                  type="button"
+                >
+                  Cancel
+                </Button>
+              </Form>
+            </div>
           </Card.Body>
         </Card>
       ))}
